a868999d3169f15624bf7eb112082fbea868792c
ali <yaghini.a110@gmail.com>
2024-07-24 13:01:52 +0330

    change file name and shebang

---
+++ phase01/solution1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+git for-each-ref --format '%(refname:short)' refs/heads/ | while read branch; do
+    echo "${branch}:"
+    git checkout "${branch}"
+    find . -type f -exec grep -H "TODO" {} +
+done
