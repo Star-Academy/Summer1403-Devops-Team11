bd594709788b77d16f4a09969efabc908913d05b
muhuchah <m.h.chah4@gmail.com>
2024-08-05 16:01:30 +0330

    use log.go instead of zap

--- phase05/main.go
+++ phase05/main.go
@@ -3,24 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"go.uber.org/zap"
 
 	"traceroute/handler"
 	"traceroute/helper"
 )
 
 func main() {
-	// Initialize Zap logger
-	logger, err := zap.NewProduction()
-	if err != nil {
-		// Fallback to a simple logger if unable to create the production logger
-		logger = zap.NewExample()
-	}
-	defer logger.Sync() // Flushes buffer
+	// Initialize logger
+	logger := helper.InitLogger()
 
 	// Load environment variables
 	if err := godotenv.Load(); err != nil {
-		logger.Warn("Error loading .env file", zap.Error(err))
+		logger.Warn("Warning: failed to load .env file")
 	} else {
 		logger.Info("Successfully loaded .env file")
 	}
@@ -30,8 +24,8 @@ func main() {
 
 	// Start the server
 	serverHost := helper.GetEnv("SERVER_HOST", ":8080")
-	logger.Info("Starting server", zap.String("host", serverHost))
+	logger.Info("Starting server")
 	if err := router.Run(serverHost); err != nil {
-		logger.Fatal("Unable to start server", zap.Error(err))
+		logger.Error("Unable to start server")
 	}
 }
