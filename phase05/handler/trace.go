749275a2a6f42d5a6790d30005da38ee65f7aa6a
muhuchah <m.h.chah4@gmail.com>
2024-08-05 16:02:00 +0330

    add logging

--- phase05/handler/trace.go
+++ phase05/handler/trace.go
@@ -10,14 +10,23 @@ import (
 )
 
 func Trace(c *gin.Context) {
+	logger := helper.InitLogger()
+
 	host := c.Param("host")
+	if host == "" {
+		logger.Warn("Invalid host")
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"ERROR": "Invalid host"})
+		return
+	}
 
 	ipAddr, err := trace.ResolveIP(host)
 	if err != nil {
+		logger.Error("IP resolution failed", "Host", host)
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"ERROR": "Failed to resolve IP address"})
 		return
 	}
 
+	logger.Info("performing Trace")
 	traceResponses := trace.PerformTrace(ipAddr)
 
 	c.IndentedJSON(http.StatusOK, traceResponses)
